519f7a1d82c469e98b70d132136b189553465e5c
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2023-02-23 17:14:42 +0530

    Update Debugging1.java

--- Java/Debugging1.java
+++ Java/Debugging1.java
@@ -6,11 +6,12 @@ class Child extends{
     static int y = x + 10;
 }
 
-class Class extends Child{
+class Object extends Child{
     static int y = y % 10;
 	public static void main(String... _){
+        Parent parent = Object();
 		System.out.println(Math.round(-2.3));
-                System.out.println(x + y);
+                System.out.println(parent.x +""+ parent.y);
 	}
 }
 
